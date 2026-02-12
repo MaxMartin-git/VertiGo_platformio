6fc93eb2fc3796cf6ae45dd16050242d0e87849b
MaxMartin-git <martin_huth@outlook.de>
2026-02-12 04:23:35 +0100

    batterieStatus für 3 Zellen angepasst

--- src/batterieStatus.cpp
+++ src/batterieStatus.cpp
@@ -11,15 +11,15 @@ void batterieStatus() {
 
   batteryVoltage = analogRead(A0) * (25.0 / 1023.0);
 
-  if      (batteryVoltage >= 8.4) batterySoC = 100;
-  else if (batteryVoltage >= 8.2) batterySoC = 90;
-  else if (batteryVoltage >= 8.0) batterySoC = 80;
-  else if (batteryVoltage >= 7.9) batterySoC = 70;
-  else if (batteryVoltage >= 7.8) batterySoC = 60;
-  else if (batteryVoltage >= 7.7) batterySoC = 50;
-  else if (batteryVoltage >= 7.6) batterySoC = 40;
-  else if (batteryVoltage >= 7.5) batterySoC = 30;
-  else if (batteryVoltage >= 7.4) batterySoC = 20;
-  else if (batteryVoltage >= 7.2) batterySoC = 10;
+  if      (batteryVoltage >= 12.60) batterySoC = 100;
+  else if (batteryVoltage >= 12.30) batterySoC = 90;
+  else if (batteryVoltage >= 12.00) batterySoC = 80;
+  else if (batteryVoltage >= 11.85) batterySoC = 70;
+  else if (batteryVoltage >= 11.70) batterySoC = 60;
+  else if (batteryVoltage >= 11.55) batterySoC = 50;
+  else if (batteryVoltage >= 11.40) batterySoC = 40;
+  else if (batteryVoltage >= 11.25) batterySoC = 30;
+  else if (batteryVoltage >= 11.10) batterySoC = 20;
+  else if (batteryVoltage >= 10.90) batterySoC = 10;
   else batterySoC = 0;
 }
